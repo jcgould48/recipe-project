4def8239c7b442ea42d2f9f06f36d75e45aaf231
Jesse Gould <jesse.gould@codeimmersives.com>
2020-03-22 12:24:06 -0400

    saves individual recipe

--- routes/admin/recipes/controllers/recipeControllers.js
+++ routes/admin/recipes/controllers/recipeControllers.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 const fetch = require('node-fetch')
-// require('dotenv').config()
+const Recipe = require('../models/Recipe')
+const flash = require('connect-flash')
 
 
 module.exports ={
@@ -74,30 +75,29 @@ recipeAPISearch:(req,res)=>{
             
         },
 
-    //     saveRecipe: (req, res, next)=>{
-           
-    //         const apiKey = `apiKey=${process.env.API_KEY}`
-    //         const url = `https://api.spoonacular.com/recipes/146963/information?${apiKey}`;
-    //         ;
-    //         fetch(url)
-    //         .then((recipe) => recipe.json())
-    //         .then((recipe) => {
-           
-    //         const newRecipe = new Recipe();
-
-    //         // newRecipe.category = req.body.category;
-    //         newRecipe.title = recipe.title;
-    //         newRecipe.image = recipe.image;
-    //         newRecipe.description = recipe.description;
-    //         newRecipe.readyInMinutes = recipe.readyInMinutes;
-    //         newRecipe.servingSize = recipe.servingSize;
-        
-    //         newRecipe.save().then(recipe => {
-    //             req.flash('message', 'Recipe saved!')
-    //             return res.redirect('/api/main/search-recipe')
-    //     })
-    // })
-    //     },
+    saveRecipe: (req, res, next)=>{
+    let id = req.params.id
+    const apiKey = `apiKey=${process.env.API_KEY}`
+    const url = `https://api.spoonacular.com/recipes/${id}/information?${apiKey}`;
+            
+            fetch(url)
+            .then((recipe) => recipe.json())
+            .then((recipe) => {
     
-
+    const newRecipe = new Recipe();
+            // newRecipe.category = req.body.category;
+            newRecipe.title = recipe.title;
+            newRecipe.image = recipe.image;
+            newRecipe.description = recipe.description;
+            newRecipe.readyInMinutes = recipe.readyInMinutes;
+            newRecipe.servings = recipe.servings;
+        
+            newRecipe.save()
+            .then(recipe => {
+                req.flash('message', 'Recipe saved!')
+                return res.redirect('/api/main/search-recipe')
+        }).catch((err) => console.log(err))
+    }) 
+},   
+   
 }
--- routes/admin/recipes/recipeRoutes.js
+++ routes/admin/recipes/recipeRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const express = require('express')
 const Recipe = require('./models/Recipe')
-const fetch = require('node-fetch')
 
-const {getSearchRecipe, recipeAPISearch, getSingleRecipe} = require('./controllers/recipeControllers')
+
+const {getSearchRecipe, recipeAPISearch, getSingleRecipe, saveRecipe} = require('./controllers/recipeControllers')
 
 router.get('/search-recipe', getSearchRecipe)
 router.get('/search', recipeAPISearch);
@@ -13,29 +13,35 @@ router.get('/single-recipe/:id', (req, res, next)=>{
     getSingleRecipe(req, res, id)
 });
 
-router.post('/single-recipe', (req, res, next)=>{
-    const apiKey = `apiKey=${process.env.API_KEY}`
-            const url = `https://api.spoonacular.com/recipes/146963/information?${apiKey}`;
-            ;
-            fetch(url)
-            .then((recipe) => recipe.json())
-            .then((recipe) => {
-    
-    const newRecipe = new Recipe();
-            // newRecipe.category = req.body.category;
-            newRecipe.title = recipe.title;
-            newRecipe.image = recipe.image;
-            newRecipe.description = recipe.description;
-            newRecipe.readyInMinutes = recipe.readyInMinutes;
-            newRecipe.servings = recipe.servings;
-        
-            newRecipe.save()
-            .then(recipe => {
-                req.flash('message', 'Recipe saved!')
-                return res.redirect('api/main/search-recipe')
-        })
-    })    
+router.post('/single-recipe/:id', (req, res, next)=>{
+    let id = req.params.id
+    saveRecipe(req, res, id)
 })
+
+// (req, res, next)=>{
+//     let id = req.params.id
+//     const apiKey = `apiKey=${process.env.API_KEY}`
+//     const url = `https://api.spoonacular.com/recipes/${id}/information?${apiKey}`;
+            
+//             fetch(url)
+//             .then((recipe) => recipe.json())
+//             .then((recipe) => {
+    
+//     const newRecipe = new Recipe();
+//             // newRecipe.category = req.body.category;
+//             newRecipe.title = recipe.title;
+//             newRecipe.image = recipe.image;
+//             newRecipe.description = recipe.description;
+//             newRecipe.readyInMinutes = recipe.readyInMinutes;
+//             newRecipe.servings = recipe.servings;
+        
+//             newRecipe.save()
+//             .then(recipe => {
+//               return  req.flash('message', 'Recipe saved!')
+//                 // return res.redirect('/api/main/search-recipe')
+//         }).catch((err) => console.log(err))
+//     })    
+// })
 
 
 
