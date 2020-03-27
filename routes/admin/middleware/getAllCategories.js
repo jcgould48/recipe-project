008786a3832468fe774efb25978c843ce3cd4565
Jesse Gould <jesse.gould@codeimmersives.com>
2020-03-27 11:49:15 -0400

    working to figure out filtering categories that appear based on user

--- routes/admin/middleware/getAllCategories.js
+++ routes/admin/middleware/getAllCategories.js
@@ -2,21 +2,22 @@ const Category = require('../categories/models/Category')
 const User = require('../../users/models/User')
 
 const getAllCategories = (req, res, next) => {
-  Category.findOne({owner: req.user._id})
-    .populate('name')
-    .exec((err, categories)=>{
-        if(err){
-            return next(err);
-        }
-        res.locals.categories = categories;
+//   let user = User._id
+//   Category.findOne({owner: user})
+//     .populate('name')
+//     .exec((err, categories)=>{
+//         if(err){
+//             return next(err);
+//         }
+//         res.locals.categories = categories;
+//       next();
+//     })
+// }  
+  Category.find({}, (err, categories)=>{
+      if(err) return next(err);
+      res.locals.categories = categories;
       next();
     })
-}  
-  // Category.find({}, (err, categories)=>{
-  //     if(err) return next(err);
-  //     res.locals.categories = categories;
-  //     next();
-  //   })
-  // }
+  }
 
   module.exports = getAllCategories;
